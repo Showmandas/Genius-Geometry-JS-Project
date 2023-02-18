d958bccbb97cd17ce6e53032c7f4fe891a592af8
Showman_Das <Showmandas3216@gmail.com>
2023-02-18 13:36:28 +0600

    rectangle and parallelogram area calculation done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let calculate=document.getElementById('calculate');
-
+let calculateTriangle=document.getElementById('calculateTriangle');
+let calculateRectangle=document.getElementById('calculateRectangle');
+let paraArea=document.getElementById('paraArea');
+let rhombusArea=document.getElementById('rhombusArea');
 // calculation for triangle 
-calculate.addEventListener('click',function(){
+calculateTriangle.addEventListener('click',function(){
 
     let b=document.getElementById('base');
     let bVal=b.value;
@@ -11,5 +13,49 @@ calculate.addEventListener('click',function(){
     let hNumVal=parseFloat(hVal);
     let triangleArea=0.5 * bNumVal * hNumVal;
     document.getElementById('result').innerText=triangleArea;
-    document.getElementById('result-body').style.display='block';
+    document.getElementById('triangle-result').style.display='block';
+})
+
+// for rectangle 
+calculateRectangle.addEventListener('click',function(){
+
+    let w=document.getElementById('width');
+    let wVal=w.value;
+    let wNumVal=parseFloat(wVal);
+    let l=document.getElementById('length');
+    let lVal=l.value;
+    let lNumVal=parseFloat(lVal);
+    let rectangleArea= wNumVal * lNumVal;
+    document.getElementById('recresult').innerText=rectangleArea;
+    document.getElementById('rectangle-result').style.display='block';
+})
+
+// for Parallelogram 
+paraArea.addEventListener('click',function(){
+
+    let b=document.getElementById('base');
+    let bVal=b.value;
+    let bNumVal=parseFloat(bVal);
+    let h=document.getElementById('height');
+    let hVal=h.value;
+    let hNumVal=parseFloat(hVal);
+    let paraArea= bNumVal * hNumVal;
+    document.getElementById('paraResult').innerText=paraArea;
+    document.getElementById('paralellogram-result').style.display='block';
+
+})
+
+// for Rhombus 
+rhombusArea.addEventListener('click',function(){
+
+    let b=document.getElementById('base');
+    let bVal=b.value;
+    let bNumVal=parseFloat(bVal);
+    let h=document.getElementById('height');
+    let hVal=h.value;
+    let hNumVal=parseFloat(hVal);
+    let paraArea= bNumVal * hNumVal;
+    document.getElementById('paraResult').innerText=paraArea;
+    document.getElementById('paralellogram-result').style.display='block';
+
 })

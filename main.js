a1c5b9fb15851586d46c86391d32485f1462aa9b
Showman_Das <Showmandas3216@gmail.com>
2023-02-18 13:56:08 +0600

    rhombus and pentagon area calculation done

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let calculateTriangle=document.getElementById('calculateTriangle');
 let calculateRectangle=document.getElementById('calculateRectangle');
 let paraArea=document.getElementById('paraArea');
 let rhombusArea=document.getElementById('rhombusArea');
+let pentagonArea=document.getElementById('pentagonArea');
+
 // calculation for triangle 
 calculateTriangle.addEventListener('click',function(){
 
@@ -48,14 +50,29 @@ paraArea.addEventListener('click',function(){
 // for Rhombus 
 rhombusArea.addEventListener('click',function(){
 
-    let b=document.getElementById('base');
-    let bVal=b.value;
-    let bNumVal=parseFloat(bVal);
-    let h=document.getElementById('height');
-    let hVal=h.value;
-    let hNumVal=parseFloat(hVal);
-    let paraArea= bNumVal * hNumVal;
-    document.getElementById('paraResult').innerText=paraArea;
-    document.getElementById('paralellogram-result').style.display='block';
+    let d1=document.getElementById('d1');
+    let d1Val=d1.value;
+    let d1NumVal=parseFloat(d1Val);
+    let d2=document.getElementById('d2');
+    let d2Val=d2.value;
+    let d2NumVal=parseFloat(d2Val);
+    let rhombusArea=0.5 *  d2NumVal * d2NumVal;
+    document.getElementById('rhombusResult').innerText=rhombusArea;
+    document.getElementById('rhombus-result').style.display='block';
+
+})
+
+// for pentagon 
+pentagonArea.addEventListener('click',function(){
+
+    let p=document.getElementById('p');
+    let pVal=p.value;
+    let pNumVal=parseFloat(pVal);
+    let b1=document.getElementById('b1');
+    let b1Val=b1.value;
+    let b1NumVal=parseFloat(b1Val);
+    let pentagonArea=0.5 *  pNumVal * b1NumVal;
+    document.getElementById('pentagonResult').innerText=pentagonArea;
+    document.getElementById('pentagon-result').style.display='block';
 
 })
